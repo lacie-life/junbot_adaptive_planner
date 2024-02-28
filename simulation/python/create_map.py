089299c98e34beb732f23c86ef36325757482f31
lacie-life <00sao00ios00@gmail.com>
2024-02-28 20:38:19 +0900

    [Add something]

--- simulation/python/create_map.py
+++ simulation/python/create_map.py
@@ -7,17 +7,21 @@ from field import guassian
 def create_map(mx, my, X, Y, SIGMA_X, SIGMA_Y, TYPE):
     n = len(X)
     Map = np.zeros((mx, my, 3))
+    Map_R = np.zeros((mx, my))
+    Map_R = Map_R + 10
+    Map_B = np.zeros((mx, my))
+    Map_B = Map_B + 100
 
     # Create the map with repulsive and attractive fields in channel 1
     for i in range(n):
         if (TYPE[i] == 0):
-            Map[:, :, 1] += rep_field(X[i], Y[i], SIGMA_X[i], SIGMA_Y[i], mx, my)
+            Map[:, :, 2] += 15 * rep_field(X[i], Y[i], SIGMA_X[i], SIGMA_Y[i], mx, my)
         elif (TYPE[i] == 2):
-            Map[:, :, 1] += att_field(X[i], Y[i], mx, my)
+            Map[:, :, 2] += att_field(X[i], Y[i], mx, my)
     
     # Color encode the map for visualization
-    Map[:, :, 2] = Map[:, :, 1] * 5
-    Map[:, :, 0] = Map[:, :, 1] * 6
+    Map[:, :, 1] = Map[:, :, 2] * 0.5
+    Map[:, :, 0] = Map[:, :, 2] * 0.2
     
     return Map
 
--- simulation/python/field.py
+++ simulation/python/field.py
@@ -11,7 +11,8 @@ def att_field(px, py, mx, my):
                 res[x, y] = t * d - 0.5 * (t ** 2)
             else:
                 res[x, y] = 0.5 * (d ** 2)
-            res[x, y] = 0.000001 * res[x, y]
+            res[x, y] = 0.0001 * res[x, y]
+            # print(res[x, y])
     return res
 
 
@@ -26,15 +27,16 @@ def rep_field(px, py, sx, sy, mx, my):
             d = math.sqrt(d)
             # Drawing the repulsive field
             if (d < 1 and d != 0):
-                vector1 = np.array([x-px, y-py])
-                vector2 = np.array([tempX-300, tempY-250])
+                vector1 = np.array([x - px, y - py])
+                vector2 = np.array([tempX - 300, tempY - 250])
                 # Tính góc giữa hai vector (trong đơn vị radian)
                 theta = np.arccos(np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
                 # if theta <= (math.pi/2):
-                res[x, y] = (300+300*(math.cos(theta)))*(1 - 1 / d) ** 2
+                res[x, y] = (300 + 300 * (math.cos(theta))) * (1 - 1 / d) ** 2
                 # else:
                 #     res[x, y] = (1 - 1 / d) ** 2
-                res[x, y] = res[x, y] * 0.001
+                res[x, y] = res[x, y] * 0.0001
+                # print(res[x, y])
             else:
                 res[x, y] = 0
     return res
