91d10fe546a675f740f30958f1292c7f1b94574b
HN <caohuynhat0209@gmail.com>
2024-03-16 21:47:23 +0700

    [test]

--- junbot_adaptive/test/adaptiveControl.cpp
+++ junbot_adaptive/test/adaptiveControl.cpp
@@ -15,17 +15,23 @@
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/server/simple_action_server.h>
 #include <custom_msgs/Obstacles.h>
-
 #include <mutex>
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 
 double calculateDistance (double Xa, double Ya, double Xb, double Yb);
 // TODO: Subscribe map infor
 
+struct Point
+{
+    float x;
+    float y;
+    float z;
+};
 
 ros::Subscriber subPlan;
 ros::Subscriber subObj;
 ros::Publisher pubObj;
-
+geometry_msgs::PoseWithCovariance amcl_pose;
 nav_msgs::Path path;
 ros::Publisher pub;
 custom_msgs::Obstacles object;
@@ -40,6 +46,11 @@ void globalPlanCallback(nav_msgs::Path::ConstPtr tempPath)
     }
     m.unlock();
 //    path = tempPath;
+}
+
+void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg_amcl)
+{
+    amcl_pose = msg_amcl->pose;
 }
 
 void objectCallback(custom_msgs::Obstacles::ConstPtr objTemp)
@@ -57,6 +68,82 @@ void objectCallback(custom_msgs::Obstacles::ConstPtr objTemp)
     m.unlock();
 }
 
+double distanceBetweenPoints(Point p1, Point p2) {
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+vector<Point> findPointsOnRectangleEdges(Point A, Point B, Point C, Point D) {
+    vector<Point> points;
+
+    int num_points_AB = distanceBetweenPoints(A, B) / 0.1;
+    int num_points_BC = distanceBetweenPoints(B, C) / 0.1;
+    int num_points_CD = distanceBetweenPoints(C, D) / 0.1;
+    int num_points_DA = distanceBetweenPoints(D, A) / 0.1;
+
+    double increment_AB_x = (B.x - A.x) / num_points_AB;
+    double increment_AB_y = (B.y - A.y) / num_points_AB;
+    for (int i = 0; i <= num_points_AB; ++i) {
+        points.push_back({A.x + i * increment_AB_x, A.y + i * increment_AB_y});
+    }
+
+    double increment_BC_x = (C.x - B.x) / num_points_BC;
+    double increment_BC_y = (C.y - B.y) / num_points_BC;
+    for (int i = 0; i <= num_points_BC; ++i) {
+        points.push_back({B.x + i * increment_BC_x, B.y + i * increment_BC_y});
+    }
+
+    double increment_CD_x = (D.x - C.x) / num_points_CD;
+    double increment_CD_y = (D.y - C.y) / num_points_CD;
+    for (int i = 0; i <= num_points_CD; ++i) {
+        points.push_back({C.x + i * increment_CD_x, C.y + i * increment_CD_y});
+    }
+
+    double increment_DA_x = (A.x - D.x) / num_points_DA;
+    double increment_DA_y = (A.y - D.y) / num_points_DA;
+    for (int i = 0; i <= num_points_DA; ++i) {
+        points.push_back({D.x + i * increment_DA_x, D.y + i * increment_DA_y});
+    }
+
+    return points;
+}
+
+double dot_product(std::vector<double>& vector1, std::vector<double>& vector2) {
+    return vector1[0] * vector2[0] + vector1[1] * vector2[1];
+}
+
+double norm(std::vector<double>& vector) {
+    return sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+}
+
+void calculate_field(Point center_point){
+    float sx = 1;
+    float sy = 1;
+    float rel = 0.1;
+    distance_center_robot = sqrt(((amcl_pose.pose.pose.position.x - center_point.x)(amcl_pose.pose.pose.position.x - center_point.x))
+    +((amcl_pose.pose.pose.position.y - center_point.y)(amcl_pose.pose.pose.position.y - center_point.y)))
+    for (float x = center_point.x+distance_center_robot; x < center_point.x-distance_center_robot; x = x - rel)
+    {
+        for (float y = center_point.y+distance_center_robot; x < center_point.y-distance_center_robot; y = y - rel)
+        {
+
+            float distance = sqrt(((amcl_pose.pose.pose.position.x - center_point.x)(amcl_pose.pose.pose.position.x - center_point.x)/(distance_center_robot*distance_center_robot))
+            +((amcl_pose.pose.pose.position.y - center_point.y)(amcl_pose.pose.pose.position.y - center_point.y)/(distance_center_robot*distance_center_robot)))
+            
+            if (d != 0){
+                vector1 = np.array([x-px, y-py])
+                vector2 = np.array([tempX-300, tempY-250])
+                double theta = acos(dot_product(vector1, vector2) / (norm(vector1) * norm(vector2)));
+            }
+                
+                res[x, y] = (200+200*(math.cos(theta)))*(1 - 1 / d) ** 2
+                # if res[x, y] > 1:
+                #     res[x, y] = 1
+            else:
+                res[x, y] = 0
+        }
+    }
+}
+
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
@@ -65,23 +152,30 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "listener");
     ros::NodeHandle n;
     subPlan = n.subscribe("/move_base/DWAPlannerROS/global_plan", 10000, globalPlanCallback);
-    
+    ros::Subscriber sub_amcl = n.subscribe("/amcl_pose", 1000, poseAMCLCallback);
     subObj = n.subscribe("/object_costmap_layer/obstacles_temp", 10000, objectCallback);
     pubObj = n.advertise<custom_msgs::Obstacles>("/object_costmap_layer/obstacles", 1000);
 
-    
-    //coner simulation
-    // float coner[4][3] = {{-0.1,0.8,0},
-    //                         {2.3,0.8,0},
-    //                         {-5,2.3,0},
-    //                         {2.35,4.15,0}};
-                            // {-6.3, 0.85, 0}
+    float coner_temp[4][3] = {{0.22,-0.6,0},
+                                {0.22,-1.8,0},
+                                {-3.6,-1.7,0},
+                                {-3.6,-0.35,0}};
     // coner experiment
     ros::Rate rate(1);
-    float coner[4][3] = {{0.22,-0.6,0},
-                         {0.22,-1.8,0},
-                         {-3.6,-1.7,0},
-                         {-3.6,-0.35,0}};
+    std::vector<Point> coner;
+    for (int i = 0; i < 4; i++)
+    {
+        Point temp;
+        temp.x = coner_temp[i][0];
+        temp.y = coner_temp[i][1];
+        coner.push_back(temp);
+    }
+    std::vector<Point> center_point = findPointsOnLine(coner.at(0), coner.at(1), coner.at(2),coner.at(3));
+    for (int i = 0; i < center_point.size(); i++)
+    {
+        calculate_field(center_point.at(i));
+    }
+                 
     while (ros::ok())
     {
         m.lock();
